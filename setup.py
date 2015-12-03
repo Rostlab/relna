ead5b15124b9117ee81c5433c09cbaef631eb0c3
Ashish Baghudana <ashish.baghudana26@gmail.com>
2015-12-03 10:32:12 +0100

    rename nala to nalaf

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license='MIT',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'nala',
+        'nalaf',
         'nltk',
         'beautifulsoup4',
         'requests',
