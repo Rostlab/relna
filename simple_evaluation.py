4437b4fac236d4dd09be53680e9f5c99ff3fa4fb
Juan Miguel Cejuela <i@juanmi.rocks>
2016-08-04 18:37:56 +0200

    simplify

--- simple_evaluation.py
+++ simple_evaluation.py
@@ -1,28 +1,38 @@
 from nalaf.utils.readers import HTMLReader
 from nalaf.utils.annotation_readers import AnnJsonAnnotationReader
 from nalaf.learning.taggers import StubSameSentenceRelationExtractor
-from nalaf.learning.evaluators import DocumentLevelRelationEvaluator
+from nalaf.learning.evaluators import DocumentLevelRelationEvaluator, Evaluations
 from relna.structures.relation_pipelines import RelationExtractionPipeline
 from relna.learning.svmlight import SVMLightTreeKernels
 from relna.preprocessing.parsers import SpacyParser, BllipParser
 from spacy.en import English
 from relna.learning.taggers import TranscriptionFactorTagger
 from relna.learning.taggers import RelnaRelationExtractor
+import argparse
 
-relna = False
-use_tk = False
-validation_set = True
+parser = argparse.ArgumentParser(description='Simple-evaluate relna corpus or LocText corpus')
 
-if use_tk:
+parser.add_argument('--corpus', required=True, choices=["relna", "LocText"])
+parser.add_argument('--use_tk', default=False, action='store_true')
+parser.add_argument('--use_test_set', default=False, action='store_true')
+
+args = parser.parse_args()
+
+print(args)
+
+# ------------------------------------------------------------------------------
+
+k = 5
+
+if args.use_tk:
     svm_folder = '/usr/local/manual/svm-light-TK-1.2.1/'
     nlp = English(entity=False)
     parser = SpacyParser(nlp, constituency_parser=True)
-    parser = BllipParser()
 else:
     svm_folder = '/usr/local/manual/bin/'
     parser = None
 
-if relna:
+if args.corpus == "relna":
     # Relna
     dataset_folder_html = '/Users/jmcejuela/Work/hck/relna/resources/corpora/relna/corrected/'
     dataset_folder_annjson = dataset_folder_html
@@ -44,42 +54,49 @@ def read_dataset():
     return dataset
 
 dataset = read_dataset()
-
 tagger = StubSameSentenceRelationExtractor('e_1', 'e_2', rel_type)
-
 evaluator = DocumentLevelRelationEvaluator(rel_type=rel_type, match_case=False)
 
-k = 5
 
+print("# FOLDS")
+merged = []
 for fold in range(k):
-    training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=validation_set)
-    if not validation_set:
+    training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=(not args.use_test_set))
+    if args.use_test_set:
         validation = test
 
     tagger.tag(validation)
 
     r = evaluator.evaluate(validation)
+    merged.append(r)
     print(r)
+
+print("\n# FINAL")
+ret = Evaluations.merge(merged)
+print(ret)
+
 
 print("\n\n\n\n\n")
 
-dataset = read_dataset()
 
+dataset = read_dataset()
+pipeline = RelationExtractionPipeline('e_1', 'e_2', rel_type, parser=parser)
 evaluator = DocumentLevelRelationEvaluator(rel_type=rel_type, match_case=False)
 
-pipeline = RelationExtractionPipeline('e_1', 'e_2', rel_type, parser=parser)
 
+print("# FOLDS")
+merged = []
 for fold in range(k):
 
-    training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=validation_set)
-    if not validation_set:
+    training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=(not args.use_test_set))
+    if args.use_test_set:
         validation = test
 
     pipeline.execute(training, train=True)
     feature_set = pipeline.feature_set
 
     # get the predictions
-    svmlight = SVMLightTreeKernels(svm_folder, use_tree_kernel=use_tk)
+    svmlight = SVMLightTreeKernels(svm_folder, use_tree_kernel=args.use_tk)
     svmlight.create_input_file(training, 'train', feature_set)
     svmlight.learn()
 
@@ -89,4 +106,9 @@ for fold in range(k):
     svmlight.read_predictions(validation)
 
     results = evaluator.evaluate(validation)
+    merged.append(results)
     print(results)
+
+print("\n# FINAL")
+ret = Evaluations.merge(merged)
+print(ret)
