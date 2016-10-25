7b2366170e2ba206a6eb3d0635c15560290c9a3f
Juan Miguel Cejuela <i@juanmi.rocks>
2016-10-25 10:57:46 +0200

    move simple_evaluation to be a test and use py.test and update relna version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,35 +3,39 @@ from setuptools import find_packages
 
 
 def readme():
-    with open('README.md') as file:
+    with open('README.md', encoding='utf-8') as file:
         return file.read()
 
 setup(
     name='relna',
-    version='0.1.1',
+    version='0.2.0',
     description='Relation Extraction Pipeline for Transcription Factor and Gene or Gene Product relations',
     long_description=readme(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Text Processing :: Linguistic'
     ],
     keywords='svm relation extraction nlp natural language ner transcription factor gene product',
     url='https://github.com/Rostlab/relna',
     author='Ashish Baghudana, Juan Miguel Cejuela',
-    author_email='abaghudana@rostlab.org',
+    author_email='i@juanmi.rocks',
+
+    include_package_data=True,
     packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+
+    test_suite='pytest-runner',
+    setup_requires=['pytest'],
+
+    dependency_links=[
+        'https://github.com/Rostlab/nalaf/tree/feature/Experimental#egg=nalaf'
+    ],
+
     install_requires=[
         # 'nalaf',
         'spacy',
         'ujson'  # It should be included with spacy, AFAIK
-    ],
-    dependency_links=[
-        'https://github.com/Rostlab/nalaf/tree/feature/Experimental#egg=nalaf'
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    test_suite='nose.collector',
-    setup_requires=['nose>=1.0'],
+    ]
 )
--- tests/test_simple_evaluation.py
+++ tests/test_simple_evaluation.py
@@ -9,111 +9,139 @@ from spacy.en import English
 # from relna.learning.taggers import TranscriptionFactorTagger
 from relna.learning.taggers import RelnaRelationExtractor
 import argparse
+import math
 
-parser = argparse.ArgumentParser(description='Simple-evaluate relna corpus corpus')
+def parse_arguments(argv):
 
-parser.add_argument('--corpus', required=True, choices=["relna"])
-parser.add_argument('--use_tk', default=False, action='store_true')
-parser.add_argument('--use_test_set', default=False, action='store_true')
-parser.add_argument('--use_full_corpus', default=False, action='store_true')
+    parser = argparse.ArgumentParser(description='Simple-evaluate relna corpus corpus')
 
-args = parser.parse_args()
+    parser.add_argument('--corpus', default="relna", choices=["relna"])
+    parser.add_argument('--use_tk', default=False, action='store_true')
+    parser.add_argument('--use_test_set', default=False, action='store_true')
+    parser.add_argument('--use_full_corpus', default=False, action='store_true')
 
-print(args)
+    args = parser.parse_args(argv)
 
-# ------------------------------------------------------------------------------
+    print(args)
 
-k = 5
-
-if args.use_tk:
-    svm_folder = ''  # '/usr/local/manual/svm-light-TK-1.2.1/' -- must be in your path
-    nlp = English(entity=False)
-    parser = SpacyParser(nlp, constituency_parser=True)
-else:
-    svm_folder = ''  # '/usr/local/manual/bin/' -- must be in your path
-    parser = None
-
-if args.corpus == "relna":
-    # Relna
-    dataset_folder_html = './resources/corpora/relna/corrected/'
-    dataset_folder_annjson = dataset_folder_html
-    rel_type = 'r_4'
+    return args
 
 
-def read_dataset():
-    dataset = HTMLReader(dataset_folder_html).read()
-    AnnJsonAnnotationReader(
-            dataset_folder_annjson,
-            read_relations=True,
-            read_only_class_id=None,
-            delete_incomplete_docs=False).annotate(dataset)
+def test_whole_with_defaults(argv=None):
+    argv = [] if argv is None else argv
+    args = parse_arguments(argv)
 
-    return dataset
+    k = 5
 
-dataset = read_dataset()
-tagger = StubSameSentenceRelationExtractor('e_1', 'e_2', rel_type)
-evaluator = DocumentLevelRelationEvaluator(rel_type=rel_type, match_case=False)
+    if args.use_tk:
+        svm_folder = ''  # '/usr/local/manual/svm-light-TK-1.2.1/' -- must be in your path
+        nlp = English(entity=False)
+        parser = SpacyParser(nlp, constituency_parser=True)
+    else:
+        svm_folder = ''  # '/usr/local/manual/bin/' -- must be in your path
+        parser = None
 
-
-print("# FOLDS")
-merged = []
-for fold in range(k):
-    training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=(not args.use_test_set))
-    if args.use_test_set:
-        validation = test
-
-    tagger.tag(validation)
-
-    r = evaluator.evaluate(validation)
-    merged.append(r)
-    print(r)
-
-print("\n# FINAL")
-ret = Evaluations.merge(merged)
-print(ret)
+    if args.corpus == "relna":
+        # Relna
+        dataset_folder_html = './resources/corpora/relna/corrected/'
+        dataset_folder_annjson = dataset_folder_html
+        rel_type = 'r_4'
 
 
-print("\n\n\n\n\n")
+    def read_dataset():
+        dataset = HTMLReader(dataset_folder_html).read()
+        AnnJsonAnnotationReader(
+                dataset_folder_annjson,
+                read_relations=True,
+                read_only_class_id=None,
+                delete_incomplete_docs=False).annotate(dataset)
 
+        return dataset
 
-if (args.use_full_corpus):
     dataset = read_dataset()
-else:
-    dataset, _ = read_dataset().percentage_split(0.1)
-
-feature_generators = RelnaRelationExtractor.default_feature_generators('e_1', 'e_2')
-pipeline = RelationExtractionPipeline('e_1', 'e_2', rel_type, parser=parser, feature_generators=feature_generators)
-
-evaluator = DocumentLevelRelationEvaluator(rel_type=rel_type, match_case=False)
+    tagger = StubSameSentenceRelationExtractor('e_1', 'e_2', rel_type)
+    evaluator = DocumentLevelRelationEvaluator(rel_type=rel_type, match_case=False)
 
 
-print("# FOLDS")
-merged = []
-for fold in range(k):
+    print("# FOLDS")
+    merged = []
+    for fold in range(k):
+        training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=(not args.use_test_set))
+        if args.use_test_set:
+            validation = test
 
-    training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=(not args.use_test_set))
-    if args.use_test_set:
-        validation = test
+        tagger.tag(validation)
 
-    # Learn
-    pipeline.execute(training, train=True)
-    svmlight = SVMLightTreeKernels(svmlight_dir_path=svm_folder, use_tree_kernel=args.use_tk)
-    instancesfile = svmlight.create_input_file(training, 'train', pipeline.feature_set)
-    svmlight.learn(instancesfile)
+        r = evaluator.evaluate(validation)
+        merged.append(r)
+        print(r)
 
-    # Predict & Read predictions
-    pipeline.execute(validation, train=False)
-    instancesfile = svmlight.create_input_file(validation, 'test', pipeline.feature_set)
-    predictionsfile = svmlight.tag(instancesfile)
-    svmlight.read_predictions(validation, predictionsfile)
+    print("\n# FINAL")
+    ret = Evaluations.merge(merged)
+    print(ret)
 
-    results = evaluator.evaluate(validation)
-    merged.append(results)
-    print(results)
+    rel_evaluation = ret(rel_type).compute(strictness="exact")
 
-print("\n# FINAL")
-ret = Evaluations.merge(merged)
-print(ret)
+    EXPECTED_F = 0.34506089309878213
+    EXPECTED_F_SE = 0.0017486985851191787
+
+    assert math.isclose(rel_evaluation.f_measure, EXPECTED_F)
+    assert math.isclose(rel_evaluation.f_measure_SE, EXPECTED_F_SE, rel_tol=0.1)
+
+    print("\n\n\n\n\n")
+
+
+    if (args.use_full_corpus):
+        dataset = read_dataset()
+    else:
+        dataset, _ = read_dataset().percentage_split(0.1)
+
+    feature_generators = RelnaRelationExtractor.default_feature_generators('e_1', 'e_2')
+    pipeline = RelationExtractionPipeline('e_1', 'e_2', rel_type, parser=parser, feature_generators=feature_generators)
+
+    evaluator = DocumentLevelRelationEvaluator(rel_type=rel_type, match_case=False)
+
+
+    print("# FOLDS")
+    merged = []
+    for fold in range(k):
+
+        training, validation, test = dataset.cv_kfold_split(k, fold, validation_set=(not args.use_test_set))
+        if args.use_test_set:
+            validation = test
+
+        # Learn
+        pipeline.execute(training, train=True)
+        svmlight = SVMLightTreeKernels(svmlight_dir_path=svm_folder, use_tree_kernel=args.use_tk)
+        instancesfile = svmlight.create_input_file(training, 'train', pipeline.feature_set)
+        svmlight.learn(instancesfile)
+
+        # Predict & Read predictions
+        pipeline.execute(validation, train=False)
+        instancesfile = svmlight.create_input_file(validation, 'test', pipeline.feature_set)
+        predictionsfile = svmlight.tag(instancesfile)
+        svmlight.read_predictions(validation, predictionsfile)
+
+        results = evaluator.evaluate(validation)
+        merged.append(results)
+        print(results)
+
+    print("\n# FINAL")
+    ret = Evaluations.merge(merged)
+    print(ret)
+
+    rel_evaluation = ret(rel_type).compute(strictness="exact")
+
+    EXPECTED_F = 0.6451612903225806
+    EXPECTED_F_SE = 0.005122878168890786
+
+    assert math.isclose(rel_evaluation.f_measure, EXPECTED_F)
+    assert math.isclose(rel_evaluation.f_measure_SE, EXPECTED_F_SE, rel_tol=0.1)
+
+
+if __name__ == "__main__":
+    import sys
+    test_whole_with_defaults(sys.argv[1:])
 
 
 # Expected output with full relna corpus (dataset = read_dataset()), something like:
